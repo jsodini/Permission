17934343ad723a47c246199eb12f161fd96ea530
James Sodini <james@sodini.io>
2018-05-18 14:07:54 -0700

    Initial Commit

---
+++ Permission/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Permission
+//
+//  Created by James Sodini on 5/18/18.
+//  Copyright © 2018 James Sodini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Permission/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Permission
+//
+//  Created by James Sodini on 5/18/18.
+//  Copyright © 2018 James Sodini. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
